0d6ba10693c8deb2f2216c3fe3dad0faa5d8cc35
이재훈 <pride_001@naver.com>
2020-11-05 06:41:30 +0000

    2

--- db.py
+++ db.py
@@ -9,6 +9,19 @@ class MysqlController:
                                     password='',
                                     db='wallet',
                                     charset='utf8')  # 데이터베이스 기본정보
-        curs = self.conn.cursor()    
-        sql = "select * from info"   
-        curs.execute(sql)
+        curs = self.conn.cursor()
+        
+    def new_node(self,id):
+        sql = "INSERT INTO info(id,ssoin) VALUES(%s,%s)"
+        self.curs.execute(sql,(id,))
+        self.conn.commit()
+        
+        
+    def mineing(self):
+        sql = "UPDATE info SET "
+    
+    
+    def trade(self,sender,recipient):
+        # 보낸이의 코인을 빼주자
+        # 받는이의 코인을 더해주자
+        sql = "UPDATE info SET sender = sender.ssoin + "
